a5946cb8c30a4a9cce4fbc2e72d614159f8486ff
LudwigNova <lnova@u.rochester.edu>
2017-01-20 14:22:18 -0500

    Some visual edits
    Comment out the filler text and button for now. Also made some slight
    changes to the css file to prevent the page from being scrollable.

--- Desktop/WebsiteProject/Javascript/backend.js
+++ Desktop/WebsiteProject/Javascript/backend.js
@@ -1,5 +1,5 @@
-var button = document.getElementById("button");
-
-button.onclick = function(){
-  alert("The button has been pressed!");
-};
+// var button = document.getElementById("button");
+//
+// button.onclick = function(){
+//   alert("The button has been pressed!");
+// };
